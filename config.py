080c54d59ab06ce24b4d2a65e968ec58b5d745eb
atrbyg24 <artwan520@gmail.com>
2026-01-06 16:20:16 -0500

    Add model names and hyperparameters

---
+++ config.py
@@ -0,0 +1,7 @@
+EMBEDDING_MODEL_NAME = "thenlper/gte-small"
+READER_MODEL_NAME = "HuggingFaceH4/zephyr-7b-beta"
+KNOWLEDGE_BASE_ID = "m-ric/huggingface_doc"
+
+CHUNK_SIZE = 512
+CHUNK_OVERLAP = 48
+TOP_K = 4
